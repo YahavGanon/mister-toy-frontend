0b244498aaaa360720b05b6cfae673ed24940ed9
YahavGanon <yahavganon86@gmail.com>
2024-03-31 08:29:38 +0300

    Changes is css at App header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -5,11 +5,15 @@ import { LoginSignup } from "./LoginSignup.jsx"
 import { logout } from "../store/actions/user.actions.js"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
 import { useTranslation } from "react-i18next"
+import { useState } from "react"
 
 export function AppHeader() {
     const [t, i18n] = useTranslation("global")
     const navigate = useNavigate()
     const user = useSelector(storeState => storeState.userModule.loggedInUser)
+    const [isOpen, setIsOpen] = useState(false)
+    const dynClass = isOpen ? 'menu-open' : ''
+    const dynBtn = isOpen ? '1' : '0'
 
     function handleChangeLanguage(lang) {
         console.log(lang)
@@ -31,26 +35,32 @@ export function AppHeader() {
             })
     }
 
+    function toggleMenu() {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <header className="header">
             <div className="first-line">
                 <div className="logo">
-                    {/* <img className="img-logo" src="https://png.pngtree.com/png-vector/20230928/ourmid/pngtree-cute-bear-teddy-toy-png-image_10149481.png" alt="" />
-                    <h1 className="h1-logo">MisterToy</h1> */}
-                  <NavLink to="/"> <img className="img-logo" src="img/MisterToy_transparent.png" alt="" /></NavLink>
+                    <NavLink to="/"> <img className="img-logo" src="img/MisterToy_transparent.png" alt="" /></NavLink>
                 </div>
-                <nav className="nav-bar">
-                    <NavLink to="/"><img className="nav-img" src="https://iconbug.com/download/size/256/icon/2897/toy-house/" alt="" /></NavLink> |<NavLink to="/toy">Toys</NavLink> |
-                    <NavLink to="/dashboard"><img className="nav-img" src="https://static-00.iconduck.com/assets.00/increase-stats-icon-2021x2048-87in2u2l.png" alt="" /></NavLink>
-                </nav>
-            </div>
+
             {user &&
-                <section style={{textAlign: "center"}}>
-                    <Link style={{textDecoration: "none", color:"#333"}} className="user-details" to={`/user/${user._id}`}>
+                <section style={{ textAlign: "center" }}>
+                    <Link style={{ textDecoration: "none", color: "#333" }} className="user-details" to={`/user/${user._id}`}>
                         Hello {user.fullname} 🪀
                     </Link>
-                    <Link style={{color: 'black'}} to={`/`} onClick={onLogout}>Logout</Link>
+                    <Link style={{ color: 'black', lineHeight: '10' }} to={`/`} onClick={onLogout}>Logout</Link>
                 </section>}
+
+                <nav className={`nav-bar ${dynClass}`}>
+                    <NavLink onClick={toggleMenu} className="nav-link" to="/"><img className="nav-img" src="https://iconbug.com/download/size/256/icon/2897/toy-house/" alt="" /></NavLink> <NavLink onClick={toggleMenu} className="nav-link" to="/toy">Toys</NavLink>
+                    <NavLink onClick={toggleMenu} className="nav-link" to="/dashboard"><img className="nav-img" src="https://static-00.iconduck.com/assets.00/increase-stats-icon-2021x2048-87in2u2l.png" alt="" /></NavLink>
+                    <button onClick={toggleMenu} style={{ position: "absolute", left: "0", top: "0", opacity: `${dynBtn}` }}>X</button>
+                </nav>
+                <button class="toggle-menu-btn" onClick={toggleMenu} type="button">☰</button>
+            </div>
 
         </header>
     )
